fc35320b385762e0ef2ef02063146aa57f947dfd
Artestasiya <anastasialoilopest@gmail.com>
2025-06-11 22:19:46 +0300

    url shorter

---
+++ URL shortener/URL shortener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CW
+{
+    class UrlShortener
+    {
+        private Dictionary<string, string> longToShort = new Dictionary<string, string>();
+        private Dictionary<string, string> shortToLong = new Dictionary<string, string>();
+        private int counter = 0;
+        private const string letters = "abcdefghijklmnopqrstuvwxyz";
+
+        public string Shorten(string longURL)
+        {
+            if (longToShort.ContainsKey(longURL))
+            {
+                return longToShort[longURL];
+            }
+
+            string shortCode = GenerateLetterCode(counter);
+            counter++;
+
+            string shortURL = "short.ly/" + shortCode;
+
+            longToShort[longURL] = shortURL;
+            shortToLong[shortURL] = longURL;
+
+            return shortURL;
+        }
+
+        public string Redirect(string shortURL)
+        {
+            if (shortToLong.TryGetValue(shortURL, out var longURL))
+            {
+                return longURL;
+            }
+            throw new Exception("URL not found");
+        }
+
+        private string GenerateLetterCode(int number)
+        {
+            string code = "";
+            do
+            {
+                code = letters[number % letters.Length] + code;
+                number = number / letters.Length - 1;
+            } while (number >= 0);
+
+            return code.Length > 4 ? code.Substring(0, 4) : code;
+        }
+    }
+}
